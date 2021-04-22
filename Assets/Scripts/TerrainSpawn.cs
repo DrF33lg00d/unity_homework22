fccabae4152f8c940e93b6b0eebaedd07cd2623e
DrF33lg00d <7drf33lg00d7@gmail.com>
2021-04-22 10:35:34 +1000

    Create scene Task4 and empty script for terrain generation

---
+++ Assets/Scripts/TerrainSpawn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+
+public class TerrainSpawn : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    private void LoadMetadata()
+    {
+        
+    }
+    
+}
